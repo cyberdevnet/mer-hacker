441c0a67f98a2cda9fd1b845ed1908ca86d64242
cyberdevnet <cyberdevnet@gmail.com>
2020-06-19 16:22:38 +0200

    added dev branch

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,77 +4,77 @@ module.exports = function (app) {
   app.use(
     "/organizations",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/networks",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/devices",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/clients",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/device_status",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/vlans",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/allVlans",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/find_ports",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/topuserdata/",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/traffic_analysis/",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );
   app.use(
     "/backup_restore/",
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://172.19.85.214:5000",
       changeOrigin: true,
     })
   );

fd661d2cd929e3608d0ba5abf5e1bd5f789bf5b7
cyberdevnet <cyberdevnet@gmail.com>
2020-08-27 10:53:39 +0000

    client usage refining

--- src/components/Tools/ClientCharts/ClientsusageHistory.js
+++ src/components/Tools/ClientCharts/ClientsusageHistory.js
@@ -3,15 +3,33 @@ import Chart from "react-apexcharts";
 import Dialog from "@material-ui/core/Dialog";
 
 export default function ClientsusageHistory(ac) {
-  const [showModal, setshowModal] = useState(false);
   const [chart, setchart] = useState(false);
-  const [refreshUsage, setrefreshUsage] = useState(false);
 
   const usageHistory = {
     options: {
       chart: {
         type: "area",
         height: 350,
+        toolbar: {
+          show: true,
+          offsetX: 0,
+          offsetY: 0,
+          tools: {
+            customIcons: [
+              {
+                icon:
+                  '<span class="glyphicon glyphicon-refresh Usagehistory"></span>',
+                index: 0,
+                title: "Refresh",
+                class: "custom-icon",
+                click: function (chart, options, e) {
+                  ac.dc.settriggerUsageHistory(ac.dc.triggerUsageHistory + 1);
+                },
+              },
+            ],
+          },
+          autoSelected: "customIcons",
+        },
       },
       dataLabels: {
         enabled: false,
@@ -21,7 +39,7 @@ export default function ClientsusageHistory(ac) {
       },
 
       title: {
-        text: "Client usage history(1 month)",
+        text: "Client usage history (1 month)",
         align: "left",
         style: {
           fontSize: "12px",
@@ -100,10 +118,6 @@ export default function ClientsusageHistory(ac) {
     ],
   };
 
-  const Cancel = () => {
-    setshowModal(false);
-  };
-
   const isFirstRunUsageHistory = useRef(true);
   useEffect(() => {
     const abortController = new AbortController();
@@ -115,7 +129,6 @@ export default function ClientsusageHistory(ac) {
     async function UsageHistory() {
       ac.dc.setflashMessages([]);
       ac.dc.setloadingUsage(true);
-      setrefreshUsage(true);
       fetch("/flask/usageHistory", {
         method: ["POST"],
         cache: "no-cache",
@@ -131,7 +144,6 @@ export default function ClientsusageHistory(ac) {
         .then((data) => {
           if (data.error) {
             ac.dc.setloadingUsage(false);
-            setrefreshUsage(false);
             ac.dc.setflashMessages(
               <div className="form-input-error-msg alert alert-danger">
                 <span className="glyphicon glyphicon-exclamation-sign"></span>
@@ -177,21 +189,17 @@ export default function ClientsusageHistory(ac) {
               series={usageHistory.series}
               type="area"
               height={350}
-              // width="500"
             />
           );
         })
         .then(() => {
-          setshowModal(true);
           ac.dc.setloadingUsage(false);
-          setrefreshUsage(false);
         });
 
       return () => {
         abortController.abort();
         ac.dc.setflashMessages([]);
         ac.dc.setloadingUsage(false);
-        setrefreshUsage(false);
       };
     }
     UsageHistory();
@@ -200,65 +208,22 @@ export default function ClientsusageHistory(ac) {
 
   return (
     <div>
-      {showModal ? (
-        <Dialog open={true} fullWidth maxWidth={"md"}>
-          <div>
-            <div className="modal-dialog-summary modal-confirm-summary">
-              <div>
-                <div
-                  style={{ borderBottom: "none", padding: 0 }}
-                  className="modal-header"
-                >
-                  <button
-                    onClick={Cancel}
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    aria-hidden="true"
-                    style={{ top: "0px", right: "-55px", outline: "none" }}
-                  >
-                    <span>&times;</span>
-                  </button>
-                  <button
-                    className="btn btn-light-refresh"
-                    onClick={() =>
-                      ac.dc.settriggerUsageHistory(
-                        ac.dc.triggerUsageHistory + 1
-                      )
-                    }
-                    type="button"
-                    disabled={refreshUsage}
-                  >
-                    {refreshUsage && (
-                      <i
-                        className="fa fa-refresh fa-spin"
-                        style={{ marginRight: "5px" }}
-                      />
-                    )}
-                    {refreshUsage && <span>Refresh</span>}
-                    {!refreshUsage && <span>Refresh</span>}
-                  </button>
-                </div>
-                <div
-                  className="modal-body text-center"
-                  style={{
-                    fontSize: "11px",
-                    color: "darkslategray",
-                    padding: 0,
-                  }}
-                >
-                  <h4>{ac.dc.clientName}</h4>
-                  <div className="row">
-                    <div className="mixed-chart">{chart}</div>
-                  </div>
-                </div>
-              </div>
+      <div className="modal-dialog-summary modal-confirm-summary">
+        <div>
+          <div
+            className="modal-body text-center"
+            style={{
+              fontSize: "11px",
+              color: "darkslategray",
+              padding: 0,
+            }}
+          >
+            <div className="row">
+              <div className="mixed-chart">{chart}</div>
             </div>
           </div>
-        </Dialog>
-      ) : (
-        <div></div>
-      )}
+        </div>
+      </div>
     </div>
   );
 }

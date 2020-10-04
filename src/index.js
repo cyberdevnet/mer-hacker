712ece1603878f8377e3576b65b6afff3fdcc4fb
cyberdevnet <cyberdevnet@gmail.com>
2020-10-04 21:51:32 +0000

    add some env vars

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import App from "./App";
 
+// disable react-dev-tools for this project
+if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
+	for (let [key, value] of Object.entries(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+		window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] = typeof value == "function" ? ()=>{} : null;
+	}
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
